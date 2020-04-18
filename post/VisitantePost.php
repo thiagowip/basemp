142399dddae86cf047cccace347424ceaac90208
Thiago <desenvolvimento3@mediaplus.com.br>
2020-04-18 08:55:41 -0300

    ajts

--- post/VisitantePost.php
+++ post/VisitantePost.php
@@ -4,38 +4,38 @@
 
 $valida = false;
 
-if( !empty($_POST) ){
+if ($_POST['email'] != "") {
 
-    $valida = true;
+  $valida = true;
 
-    @require_once("controller/VisitanteController.class.php");
-    $VisitanteController = new VisitanteController();
-    $visitante = new Visitante();
-    $visitante->nome = addslashes(utf8_decode($_POST["nome"]));
-    $visitante->email = addslashes(utf8_decode($_POST["email"]));
-    $visitante->dt = date('d/m/Y');
-    $VisitanteController->salvar($visitante);
+  // @require_once("controller/VisitanteController.class.php");
+  // $VisitanteController = new VisitanteController();
+  // $visitante = new Visitante();
+  // $visitante->nome = addslashes(utf8_decode($_POST["nome"]));
+  // $visitante->email = addslashes(utf8_decode($_POST["email"]));
+  // $visitante->dt = date('d/m/Y');
+  // $VisitanteController->salvar($visitante);
 
-    //$retorno = explode("*",)[1];
+  //$retorno = explode("*",)[1];
 
-    $data = array(
-      'error' => '',
-      'message' => 'Cadastro efetivado!',
-      'complement' => 'Clique em ok para continuar',
-      'callback' => '  '
-    );
+  $data = array(
+    'message' => 'Cadastro efetivado',
+    'complement' => 'Clique em ok para continuar',
+    'callback' => '  '
+  );
 
-    echo json_encode($data);
-
-//	$_POST['Te ligamos'] = 'Ligar para este telefone';
-//	require_once("post/EmailPost.php");
+  //	$_POST['Te ligamos'] = 'Ligar para este telefone';
+  //	require_once("post/EmailPost.php");
 
 }
 
-if($valida == false){ 
- 
-    @header("location:$mainFolder/home");
+if ($valida == false) {
 
+  $data = array(
+    'message' => 'Erro ao cadastrar!',
+    'complement' => 'Clique em ok para continuar',
+    'callback' => '  '
+  );
 }
 
-?>
+echo json_encode($data);
