774a1110373cae495b8fd0df565f3049439687dc
Thiago <desenvolvimento3@mediaplus.com.br>
2020-04-18 05:22:34 -0300

    ajts

--- includes/lib/mp-ajax.js
+++ includes/lib/mp-ajax.js
@@ -1,11 +1,124 @@
-function ajax (page = "", params = false, timeout = 0, dinamico = false, id = "") {
-  setTimeout(function() {
+/*
+MP-FORM-AJAX
+@# Dependencias (Sweet)
+lib/jquery.sweetalert2.css
+lib/jquery.sweetalert2.js
+*/
+var $;
+var swal;
+$(document).ready(function () {
+  //SELECTS
+  $("div[mp-select] select ").change(function () {
+    var rec = $(this)
+      .parent("div[mp-select]")
+      .find("strong");
+    var val = $(this).val();
+    $(this)
+      .find("option")
+      .each(function (index, element) {
+        if ($(element).val() == val) {
+          rec.html($(element).html());
+        }
+      });
+  });
+  $("div[mp-select]")
+    .find("select")
+    .each(function (index, element) {
+      var rec = $(element)
+        .parent("div[mp-select]")
+        .find("strong");
+      var val = $(element).val();
+      $(element)
+        .find("option")
+        .each(function (i, el) {
+          if ($(el).val() == val) {
+            rec.html($(el).html());
+          }
+        });
+    });
+
+  //AJAX REQUEST
+  $.mpAjax = function (path, params, content) {
+    $.ajax({
+      url: $.mediaplus.mainFolder + "/ajax/" + path + ".ajax",
+      type: "POST",
+      data: $.param(params),
+      beforeSend: function () {
+        $("body").prepend("<div mp-load></div>");
+      },
+      success: function (data) {
+        $("div[mp-load]").fadeOut("slow", function () {
+          $("div[mp-load]").remove();
+
+          if (data) {
+            $(content).html(data);
+          }
+        });
+      },
+      error: function () {
+        console.log("Error request");
+      }
+    });
+  };
+
+  //FORM SUBMIT AJAX
+  $("*[mp-form]").ajaxForm({
+    dataType: "html",
+    type: "post",
+    beforeSend: function () {
+      $("body").prepend("<div mp-load></div>");
+    },
+    success: function (data) {
+      data = $.parseJSON(data);
+
+      $("div[mp-load]").fadeOut("slow", function () {
+        $("div[mp-load]").remove();
+
+        if (!data.error) {
+          swal(
+            {
+              title: data.message,
+              text: data.complement,
+              type: "error",
+              confirmButtonText: "Ok"
+            },
+            function () {
+              eval(data.callback);
+            }
+          );
+        } else {
+          $("form[mp-form]").resetForm();
+
+          swal(
+            {
+              title: data.message,
+              text: data.complement,
+              type: "success",
+              confirmButtonText: "Ok"
+            },
+            function () {
+              eval(data.callback);
+            }
+          );
+        }
+      });
+    },
+    error: function () {
+      swal("Falha ao enviar dados!", "Clique no botão para sair", "error");
+    }
+  });
+});
+
+// funcao ajax sem dependencias
+
+function ajax(page = "", params = false, timeout = 0, dinamico = false, id = "") {
+  setTimeout(function () {
     $.ajax({
       url: window.mainFolder + "/ajax/" + page + ".ajax",
       type: "POST",
       data: params,
-      beforeSend: function() {},
-      success: function(data) {
+      beforeSend: function () { },
+      success: function (data) {
         if (dinamico === true) {
           if (id) {
             $(".content-data-" + page + id).html(data);
@@ -17,3 +130,4 @@ function ajax (page = "", params = false, timeout = 0, dinamico = false, id = ""
     });
   }, timeout);
 }
+
--- template-site/header.php
+++ template-site/header.php
@@ -26,30 +26,47 @@ function mp_url($type)
   <?php include "meta-tags.php"; ?>
 
   <link type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-  <link type="text/css" href="<?php echo $mainFolder; ?>/includes/lib/lightgallery.css?01891" rel="stylesheet">
+  
+
+  <!-- //libs -->
   <link type="text/css" href="<?php echo $mainFolder; ?>/includes/lib/foundation.min.css" rel="stylesheet">
-  <link type="text/css" href="https://unpkg.com/swiper/css/swiper.min.css" rel="stylesheet">
-  <link type="text/css" href="<?php echo $mainFolder; ?>/includes/css/app.css" rel="stylesheet">
+  <link type="text/css" href="<?php echo $mainFolder ?>/includes/lib/swiper.min.css" rel="stylesheet">
   <link type="text/css" href="<?php echo $mainFolder; ?>/includes/lib/fallback.css" rel="stylesheet">
   <link type="text/css" href="<?php echo $mainFolder; ?>/includes/lib/jquery.sweetalert2.css" rel="stylesheet">
 
+  <link type="text/css" href="<?php echo $mainFolder; ?>/includes/css/app.css" rel="stylesheet">
+
   <script src="<?php echo $mainFolder ?>/includes/lib/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-  <?php /* <script type="text/javascript" src="<?php //echo $mainFolder; ?> /includes/lib/moment.js"></script>-->
-  <!--  <script type="text/javascript" src="<?php //echo $mainFolder; ?>/includes/lib/calendario.js"></script>--> */ ?>
+  <script src="<?php echo $mainFolder ?>/includes/lib/jquery.mask.min.js"></script>
+
+  <?php 
+  /* 
+  //calendario
+  <script type="text/javascript" src="<?php //echo $mainFolder; ?> /includes/lib/moment.js"></script>-->
+  <script type="text/javascript" src="<?php //echo $mainFolder; ?>/includes/lib/calendario.js"></script>--> 
+  */ 
+  ?>
+
   <script src="<?php echo $mainFolder; ?>/includes/lib/wow.js"></script>
   <script src="<?php echo $mainFolder; ?>/includes/lib/foundation.min.js"></script>
-  <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+  <script src="<?php echo $mainFolder ?>/includes/lib/swiper.min.js"></script>
+
+  <!-- //scripts projeto -->
   <script src="<?php echo $mainFolder; ?>/includes/js/core.js"></script>
   <script src="<?php echo $mainFolder; ?>/includes/js/app.js"></script>
-  <!-- <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.sweetalert2.js"></script> -->
-  <!-- <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.form.js"></script> -->
-  <!-- <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/mp-ajax.js"></script> -->
+
+  
+  <!-- //form ajax -->
+  <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.sweetalert2.js"></script>
+  <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.form.js"></script>
+    
+  <!-- //scripts mp -->
+  <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/mp-ajax.js"></script>
   <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/mp-lightbox.js"></script>
   <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/mp-utils.js"></script>
 
   <script type="text/javascript">
-    new WOW().init();
+    // new WOW().init();
 
     window.mainFolder = '<?php echo $mainFolder; ?>';
     window.mpPage = '<?php echo ucfirst(@$page); ?>';
