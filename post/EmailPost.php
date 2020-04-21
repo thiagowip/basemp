59c9137e18c4dcdc717d80d02661ce152a199758
thiago <thiagowip@gmail.com>
2020-04-21 08:44:48 -0300

    :hammer: Alteração EmailPost

--- post/EmailPost.php
+++ post/EmailPost.php
@@ -1,43 +1,63 @@
 <?php
 @header("Content-Type: text/html; charset=ISO-8859-1", true);
+@include "implement/Encoding.php";
 
-@require_once("recaptchalib.php");
+use ForceUTF8\Encoding as Encoding;
 
-$secret = "6LdFfeoUAAAAAG5IVX5TDk4jAHEQwt4wG_QI8yA4";
-$response = null;
-$reCaptcha = new ReCaptcha($secret);
+@$_POST["g-recaptcha-response"] = $_POST["g-recaptcha-response"];
 
-$url = 'https://www.google.com/recaptcha/api/siteverify';
-$data = array(
-  'secret' => $secret,
-  'response' => $_POST["g-recaptcha-response"],
-  'remoteip' => $_SERVER['REMOTE_ADDR']
-);
+if (!$_POST['g-recaptcha-response']) {
+  @require_once("recaptchalib.php");
+  $secret = "6LdFfeoUAAAAAG5IVX5TDk4jAHEQwt4wG_QI8yA4";
+  $response = null;
+  $reCaptcha = new ReCaptcha($secret);
 
-$curlConfig = array(
-  CURLOPT_URL => $url,
-  CURLOPT_POST => true,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_POSTFIELDS => $data
-);
-
-$ch = curl_init();
-curl_setopt_array($ch, $curlConfig);
-$response = curl_exec($ch);
-curl_close($ch);
-
-$jsonResponse = json_decode($response);
-
-if ($_POST["g-recaptcha-response"]) {
-  $response = $reCaptcha->verifyResponse(
-    $_SERVER["REMOTE_ADDR"],
-    $_POST["g-recaptcha-response"]
+  $url = 'https://www.google.com/recaptcha/api/siteverify';
+  $data = array(
+    'secret' => $secret,
+    'response' => $_POST["g-recaptcha-response"],
+    'remoteip' => $_SERVER['REMOTE_ADDR']
   );
+
+  $curlConfig = array(
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => $data
+  );
+
+  $ch = curl_init();
+  curl_setopt_array($ch, $curlConfig);
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  $jsonResponse = json_decode($response);
+
+  if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+      $_SERVER["REMOTE_ADDR"],
+      $_POST["g-recaptcha-response"]
+    );
+  }
 }
 
-if ($_POST) {
+// sem recaptcha
+if ($_POST['g-recaptcha-response'] == "") {
+  $data = array(
+    'error' => '3',
+    'message' => '' . Encoding::fixUTF8('Solicitação enviado com sucesso') . '',
+    'complement' => 'Em breve retornaremos',
+    'callback' => '  '
+  );
+
+  echo json_encode($data);
+}
+
+// com recaptcha
+if ($_POST && $_POST['g-recaptcha-response']) {
   require_once "implement/SendGrid.php";
   $send = new SendgridImplement();
+
   if ($jsonResponse->success === true) {
     $email_retorno = $_POST['email'];
     $titulo = "";
@@ -51,7 +71,6 @@ if ($_POST) {
         }
       }
     }
-
 
     @require_once("controller/GeralController.class.php");
     $GeralController = new GeralController();
